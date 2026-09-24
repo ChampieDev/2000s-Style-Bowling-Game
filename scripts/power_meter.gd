b32858791736d6a94a868a24ef21d993ea8ddb09
ChampieDev <champiepier@gmail.com>
2026-09-24 08:41:54 -0400

    trying out weird circle/horizontal progress bar

--- scripts/power_meter.gd
+++ scripts/power_meter.gd
@@ -1,11 +1,18 @@
 extends Control
 
+@onready var h_bar: TextureProgressBar = $TextureProgressBar
+@onready var c_bar: TextureProgressBar = $TextureProgressBar2
+
+@export var p_value: int = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$anims.play("start")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	h_bar.value = p_value
+	c_bar.value = p_value
+	
+	
+ 
